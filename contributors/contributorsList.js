4fb1410ad6e7dfa2232dc8528d5b4fb72b66861e
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2022-10-13 19:35:41 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2029,6 +2029,10 @@ contributors = [
     fullname: "Arshpreet Singh Bedi",
     username: "https://github.com/avibedi1768",
   },
-
+ {
+    id: 419,
+    fullname: "Raghav Bajaj",
+    username: "https://github.com/Raghav-Bajaj",
+  },
 
 ];
